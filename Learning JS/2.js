7c84209685d310863ae991518e3eb769530e864a
MogaS811 <amogh2710@gmail.com>
2026-07-21 22:15:14 +0000

    Datatypes ke baare mea jana

--- Learning JS/2.js
+++ Learning JS/2.js
@@ -1,1 +1,22 @@
 "use strict" // Treat all code as a newer version.
+// alert(3+3) can not use unless you use console in browser or in html, node.js karne ke liye different syntax is there.
+
+// Use semi colon when writing 2 statements in a single line
+
+/* Data types par discussion.
+1. number => 2 to power 53 (range of a number)
+2. bigint
+3. string => "" i.e use double commas for it
+4. Boolean (true/false)
+5. Null - standalone value
+6. Undefined - Abhi value define nahi hui hai
+7. symbol - uniqueness ke liye use these
+*/
+
+let name1 = 18
+let name2 = "Amogh"
+let name3 = true
+
+console.log(typeof name1)
+console.log(typeof name2)
+console.log(typeof name3)
